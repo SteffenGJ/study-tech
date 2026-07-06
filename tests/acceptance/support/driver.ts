c9087883e74e135b57e017b8e1e7984a0ca90842
Steffen Georgi <steffen.jensen@hafnet.dk>
2026-07-06 15:27:30 +0200

    fix import errors

--- tests/acceptance/support/driver.ts
+++ tests/acceptance/support/driver.ts
@@ -1,5 +1,5 @@
 import { Builder, type WebDriver } from 'selenium-webdriver'
-import chrome from 'selenium-webdriver/chrome'
+import { Options as ChromeOptions } from 'selenium-webdriver/chrome'
 
 export function appUrl(path = '/'): string {
   const baseUrl = process.env.TEST_BASE_URL ?? 'http://127.0.0.1:5173'
@@ -12,13 +12,15 @@ export async function createDriver(): Promise<WebDriver> {
   const builder = new Builder().forBrowser(browser)
 
   if (browser === 'chrome' && process.env.SELENIUM_HEADLESS !== 'false') {
-    builder.setChromeOptions(
-      new chrome.Options().addArguments(
-        '--headless=new',
-        '--disable-gpu',
-        '--window-size=1280,720',
-      ),
+    const options = new ChromeOptions()
+
+    options.addArguments(
+      '--headless=new',
+      '--disable-gpu',
+      '--window-size=1280,720',
     )
+
+    builder.setChromeOptions(options)
   }
 
   return builder.build()
